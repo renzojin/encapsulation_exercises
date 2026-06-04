0311210127edd8c31392392d2bbd693509a71f5f
renzojin <jeanneagustin09@gmail.com>
2026-06-04 12:59:25 +0800

    feat(car): Add getters and settters for year_model and make.

--- car_class.py
+++ car_class.py
@@ -11,4 +11,21 @@ class Car:
         self.__make = make
         self.speed = 0
 
+    # Year model getter and setter
+    def get_year_model(self):
+        return self.__year_model
+
+    def set_year_model(self, year_model):
+        self.__year_model = year_model
+
+    # Make getter and setter
+    def get_make(self):
+        return self.__make
+
+    def set_make(self, make):
+        self.__make = make
+
+    # Speed getter (no setter - speed only changes via accelerate/brake)
+    def get_speed(self):
+        return self.__speed
     
